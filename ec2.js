df3688f928a4ed95bb84476544e68e4bc13899cc
Anoop Kumar <anoop_kr@yahoo.com>
2019-02-19 17:29:59 +0530

    Integrated transformer. Runs asynchronously, so not quiet right.

--- ec2.js
+++ ec2.js
@@ -2,7 +2,6 @@
 const AWS = require('aws-sdk');
 
 module.exports.startEC2 = (event, context, callback) => {
-	console.log("wd.kjfbvjk;dbnfvkjdsfkj;n");
   const ec2 = new AWS.EC2();
   const params = {
     InstanceIds: [
